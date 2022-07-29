9ee28dc4bdba64e58f9744c410927cda8d4017bf
mason madaras <m.madaras@masons-MacBook-Air.local>
2022-07-29 14:44:22 -0400

    update

--- source/dbconnect.js
+++ source/dbconnect.js
@@ -2,7 +2,7 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { credentials } from "../credentials.js";
 
-export function dbConnect(){
+export function dbConnect(){ 
     if(!getApps().length){
         initializeApp({
             credential: cert(credentials)
